f767badc67baf5840f1a127aaf05eb799d4091f5
Tyler Robbins <tyler@familyrobbins.com>
2014-01-12 14:33:29 -0800

    Final Commit

---
+++ Numgame.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+	int choice;
+	int mystnum;
+	int turn = 0;
+	int c = 0;
+
+	srand(time(NULL));
+	mystnum = (rand() % 100)+1;
+
+	while(turn < 10 && choice != mystnum){
+		cout << "Please enter a number between 1 and 100: ";
+		cin >> choice;
+
+		if(choice > mystnum){
+			cout << "Too high!\n";
+		}else if(choice < mystnum){
+			cout << "Too low!\n";
+		}
+
+		turn++;
+	}
+
+	if(choice == mystnum){
+		cout << "Congratulations! You guessed the number in " << turn << " turns!\n";
+
+	}else if(choice != mystnum){
+		cout << "Too bad. The correct number was " << mystnum << ".\n";
+
+	}
+	return 0;
+}
+
+char nuclear = '\u2622';
